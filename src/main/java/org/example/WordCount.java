d282f52128fa44b631696692dfeace50ee30e56e
Don Tay Xi Cheng <dontay0209@gmail.com>
2022-02-21 21:15:40 +0800

    Update vscode launch for topkcommonwords

--- src/main/java/org/example/WordCount.java
+++ src/main/java/org/example/WordCount.java
@@ -28,7 +28,6 @@ public class WordCount {
       StringTokenizer itr = new StringTokenizer(value.toString());
       while (itr.hasMoreTokens()) {
         word.set(itr.nextToken());
-        System.out.println(word.toString());
         context.write(word, one);
       }
     }
